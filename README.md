222e31e7bd4c417fadca3f0ab2f445f5d98eaf1f
enrico69 <e.courtial30@gmail.com>
2018-01-05 10:11:27 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# magento2-redirect-on-login
-Magento 2: redirect customer on login
---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Eric COURTIAL <e.courtial30@gmail.com>
+ * Date: 04/01/2018
+ * Time: 15:50
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Enrico69_Magento2RedirectOnLogin',
+    __DIR__
+);
